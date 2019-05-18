5235af74289889be14f3a64671b52e972e42b88d
Felipe <felipeejunges@hotmail.com>
2019-05-18 16:38:05 -0300

    Primeiro Commit

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{ 
+    public GameObject bulletRef;
+    public Transform spawnPointRef;
+    public float shootForce = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject aux;
+            aux = Instantiate(bulletRef, 
+                spawnPointRef.position, 
+                spawnPointRef.rotation);
+            aux.GetComponent<Rigidbody>().AddForce(spawnPointRef.right * (shootForce * ( - 1)),
+                ForceMode.Impulse);
+        }
+    }
+}
